c70c5cdda538a10b7db82a4b65fd66457a8569ee
Ben Masi <ben.m.masi@gmail.com>
2014-10-29 16:13:52 -0400

    Can search hotels by name and will return hotel information with headers printed

--- hotels/searcher.rb
+++ hotels/searcher.rb
@@ -3,14 +3,34 @@ require "csv"
 class Searcher
 
   def initialize
-    hotels = []
+    @hotels = []
     CSV.foreach('hotels.csv', headers: true) do |hotel|
-      hotels << hotel
+      @hotels << hotel
     end
-
-    puts hotels
   end
+
+  def find_hotel
+    ask_user_for_hotel
+    return_hotel_info
+  end
+
+  def ask_user_for_hotel
+    puts "Which hotel would you like to stay at?"
+    @user_query = gets.chomp
+    puts @user_query
+  end
+
+  def return_hotel_info
+    found_hotel = @hotels.find {|hotel| hotel["Hotel"] == @user_query.to_s}
+    puts "Property: #{found_hotel["Hotel"]}"
+    puts "City: #{found_hotel["City"]}"
+    puts "Phone Number: #{found_hotel["Phone Number"]}"
+    puts "Number of Singles: #{found_hotel["Number of Singles"]}"
+    puts "Number of Doubles: #{found_hotel["Number of Doubles"]}"
+  end
+  
 end
 
 searcher = Searcher.new
+searcher.find_hotel
 
